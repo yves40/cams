785bed77aa0fa8f615b323a97930b00cd9838187
yves40 <yves.toubhans@free.fr>
2019-03-12 17:07:34 +0100

    Just code format

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,14 @@
 //    Jan 31 2019   Logout route
 //    Feb 21 2019   Helloworld changed to Welcome
 //    Mar 11 2019   Reorg paths code
+//    Mar 12 2019   Code reorg
 //----------------------------------------------------------------------------
 
-const Version = "index.js: Mar 11 2019, 1.15 ";
+const Version = "index.js: Mar 12 2019, 1.16 ";
 
 import Vue from "vue";
 import Router from "vue-router";
 import Welcome from "@/components/Welcome";
-import Login from "@/components/Login";
-import Register from "@/components/Register";
-import Identity from "@/components/Identity";
-import Logout from "@/components/Logout";
 
 Vue.use(Router);
 
@@ -24,6 +21,12 @@ const mainroutes = [
       { path: "/", name: "Welcome",component: Welcome },
       { path: "/", name: "Home", component: Welcome },
 ];
+
+import Login from "@/components/Login";
+import Register from "@/components/Register";
+import Identity from "@/components/Identity";
+import Logout from "@/components/Logout";
+
 const userRoutes = [
   {path: "/users/register", name: "Register", component: Register },
   {path: '/users/login', name: 'Login', component: Login, },
