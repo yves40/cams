a6aee95f150cc17c91ab0c3c9f4a020598243652
yves40 <yves.toubhans@free.fr>
2019-03-28 10:52:11 +0100

    Works on criterias

--- testbed/mongologreader.js
+++ testbed/mongologreader.js
@@ -5,7 +5,7 @@
 //    Mar 28 2019    Query problems
 //----------------------------------------------------------------------------
 
-const Version = "mongologreader.js:1.09 Mar 28 2019 ";
+const Version = "mongologreader.js:1.12 Mar 28 2019 ";
 
 const mongo = require('../src/utilities/mongo');
 const helpers = require('../src/utilities/helpers');
@@ -15,7 +15,8 @@ const Mongolog = require ('../src/models/mongoLogModel');
 const ObjectId = require('mongodb').ObjectId;
 
 let loglimit = null;
-let timelimit = null;
+let uppertimelimit = null;
+let lowertimelimit = null;
 let modulename = null;
 //----------------------------------------------------------------------------
 // Parse command line args
@@ -30,18 +31,26 @@ function parseCommandLine() {
         let value = param.split('=')[1];
         if (value !== undefined)
           switch(keyword) {
-            case '-t':  if (value.length < 6) {
+            case '-ut': if (value.length < 6) {
                           // Expect user specified just hh:mm, so add current day month year
                           value = helpers.getDate() + ' ' + value;                       
                         }
-                        timelimit = new Date(value);
-                        logger.info(Version + 'Time limit set to : ' + helpers.convertDateTime(timelimit));
+                        uppertimelimit = new Date(value);
+                        logger.info(Version + 'Upper time limit set to : ' + helpers.convertDateTime(uppertimelimit));
                         validparam = true;
                         break;
-            case '-l':  loglimit = parseInt(value);
-                        logger.info(Version + 'Will report no more than ' + loglimit + ' lines');
-                        validparam = true;
-                        break;
+          case '-lt': if (value.length < 6) {
+                        // Expect user specified just hh:mm, so add current day month year
+                        value = helpers.getDate() + ' ' + value;                       
+                      }
+                      lowertimelimit = new Date(value);
+                      logger.info(Version + 'Lower time limit set to : ' + helpers.convertDateTime(lowertimelimit));
+                      validparam = true;
+                      break;
+          case '-l':  loglimit = parseInt(value);
+                      logger.info(Version + 'Will report no more than ' + loglimit + ' lines');
+                      validparam = true;
+                      break;
             case '-m':  modulename = value;
                         logger.info(Version + 'Searching for module : ' + modulename)
                         validparam = true;
@@ -53,20 +62,25 @@ function parseCommandLine() {
         }
       }
   });
-  
+  if((lowertimelimit && uppertimelimit)&&(lowertimelimit > uppertimelimit)) {
+      throw new Error('Cannot set a time range with most recent time ' + helpers.convertDateTime(uppertimelimit) +
+             ' less than oldest time' + helpers.convertDateTime(lowertimelimit));
+  }
 }
 //----------------------------------------------------------------------------
 // ussage
 //----------------------------------------------------------------------------
 function usage() {
-  console.log('\n');
-  console.log('Usage : node mongologreader [-l=maxlog] [-m=modulename] -t=<valid-date>');
+  console.log('\n\n');
+  console.log('Usage : node mongologreader [-l=maxlog] [-m=modulename] [-ut=<valid-date>] [-ut=<valid-date>]');
   console.log('[] maxlog is the xaximum number of log events reported.');
   console.log('[] modulename is the name of a module which logged in mongo repository.');
-  console.log('[] valid-date defines the latest date to consider. All events posted before this date will not be read.');
+  console.log('[] -ut specifies the most recent time to be considered');
+  console.log('[] -lt specifies the oldest time to be considered');
+  console.log('[]     valid-date defines the latest date to consider. All events posted before this date will not be read.');
   console.log('[]            Format must be either \"mon-dd-yyyy hh:mm\". or hh:mm');
   console.log('[]            Notice the surrounding \"\" when a full date is specified');
-  console.log('\n');
+  console.log('\n\n');
 }
 //----------------------------------------------------------------------------
 // Go
@@ -75,8 +89,9 @@ try {
   parseCommandLine();
 }
 catch(Error) {
-    console.log('********** Error : ' + Error);
+    console.log('\n\n********** Error : ' + Error);
     usage();
+    process.exit(1);
 }
 // Get a connection
 mongo.getMongoDBConnection();
@@ -87,9 +102,13 @@ query.select('module message timestamp severity').sort({timestamp: -1});  // Sor
 if (modulename !== null) {
   query.select().where('module').equals(modulename); 
 }
+// Any recent time ? 
+if(uppertimelimit) {  
+  query.select().where('timestamp').gt(uppertimelimit);
+}
 // Any oldest time ? 
-if(timelimit) {
-  query.select().where('timestamp').gt(timelimit);
+if(lowertimelimit) {  
+  query.select().where('timestamp').lt(lowertimelimit);
 }
 // Any limit to number of lines ?
 if (loglimit) {
