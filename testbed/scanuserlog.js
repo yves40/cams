dec61049634dc011a9086bcc4e294904e0846cbb
yves40 <yves.toubhans@free.fr>
2019-04-04 17:34:35 +0200

    Broken code

--- testbed/scanuserlog.js
+++ testbed/scanuserlog.js
@@ -2,9 +2,10 @@
 //    scanuserlog.js
 //
 //    Apr 03 2019    Initial, from simplemongologgertest
+//    Apr 04 2019    Add info, qualifiers and format listing
 //----------------------------------------------------------------------------
 
-const Version = "scanuserlog.js:1.06 Apr 03 2019 ";
+const Version = "scanuserlog.js:1.12 Apr 04 2019 ";
 
 const objectid = require('mongodb').ObjectId;
 const mongoose = require('mongoose');
@@ -18,7 +19,9 @@ console.log('\n\n');
 logger.infos(Version + 'Start search ');
 
 let useremail = undefined;
-let userid = undefined;
+let loglimit = undefined;
+let verbose = true;
+let userids = [];       // All users to get log from 
 let validparam = false;
 //----------------------------------------------------------------------------
 // Parse command line args
@@ -38,12 +41,22 @@ function parseCommandLine() {
                     useremail = value;
                     validparam = true;
                     break;
+        case '-l':  
+                    value = process.argv[++index];
+                    if (value === undefined) {
+                      throw new Error('You specified ' + keyword + ' without any value');
+                    }
+                    loglimit = parseInt(value);
+                    validparam = true;
+                    break;
+        case '-s':  verbose = false;   // Silent mode ?
+                    validparam = true;
+                    break;
       }
       if (!validparam) {throw new Error('Invalid parameter : ' + keyword);}
       ++index;
       value = keyword = undefined; // Next loop
     }
-    if (useremail === undefined) {throw new Error('No user email specified, provide one please');}
 };
 
 
@@ -53,7 +66,9 @@ function parseCommandLine() {
 function usage() {
 
     console.log('\n\n');
-    console.log('Usage : node scanuserlog -mail <usermail> \n');
+    console.log('Usage : node scanuserlog [-mail <usermail>] [-l maxlog] [-s] \n');
+    console.log('[] maxlog is the maximum number of log events reported.');
+    console.log('[] -s silent mode');
     console.log('\n\n');
 }
   
@@ -63,56 +78,74 @@ function usage() {
 try {
 
     parseCommandLine();
-    logger.infos('Searching log history for user : ' + useremail);
+    if (verbose&&useremail) logger.infos('Searching log history for user : ' + useremail);
     // Get a connection
     mongo.getMongoDBConnection();
-    // Run
+    // Builds the query to find the user
+    let query = User.find({});
+    query.select('email'); 
+    if(useremail) query.select().where('email').equals(useremail);
+    // Search users
     (async() => {
-        // Builds the query to find the user
-        let query = User.find({});
-        query.select('email').where('email').equals(useremail);
-        await query.exec(function(err, thelist) {
-            if (err) console.log(err);
-            if(thelist.length === 0) {
-                logger.error('No matching user for ' + useremail);
-            }
-            else {
-                thelist.forEach((value, index) => {
-                    userid = value._id;
-                    console.log('\n[ %s ] %s %s', ('000'+index).slice(-3), 
-                        value.email,
-                        (value._id.toString(16).substr(-32)).toUpperCase()
-                        );
-                    });
-            }
-            logger.infos('Browse the userlogs collection with user ID : ' + userid);
-            (async() => {
-                let querylog = userLog.find({});
-                querylog.select('email action timestamp ip severity').sort({timestamp: -1});
-                querylog.select().where('userid').equals(userid);
-                await querylog.exec( function (err, loglist) {
-                    if (err) console.log(err);
-                    logger.infos('Found ' + loglist.length + ' entries\n\n');
-                    loglist.forEach((value, index) => {
-                        // Some formatting
-                        let IP = 'Not collected'.padStart(24, ' ');
-                        if(value.ip) {
-                            IP = value.ip.padStart(24, ' ');
-                        }
-                        console.log('%s %s %s %s', helpers.convertDateTime(value.timestamp), 
-                                    IP,
-                                    value.action.padStart(35, ' '),
-                                    value.email);
-                    });
-                    process.exit(0);    
-                });
-            })();
-        });
+        let userids = await getUserIds(query);
+        console.log('userids contains ' + userids.length + ' entries');
+        await getUserLogs(userids);
+        // process.exit(0);
     })();
 }
 catch(Error) {
     console.log('\n\n********** Error : ' + Error);
     usage();
     process.exit(1);
+}
+//----------------------------------------------------------------------------
+// Get user ID list
+//----------------------------------------------------------------------------
+async function getUserIds(query) {
+    await query.exec(function(err, thelist) {
+        if (err) console.log(err);
+        if(thelist.length === 0) {
+            logger.error('No matching user for ' + useremail);
+        }
+        else {
+            thelist.forEach((value, index) => {
+                userids.push(value._id);
+                console.log('\n[ %s ] %s %s', ('000'+index).slice(-3), 
+                    value.email,
+                    (value._id.toString(16).substr(-32)).toUpperCase()
+                    );
+            });
+            return new Promise(resolve => { resolve(userids)});
+        }
+    });
+}
+//----------------------------------------------------------------------------
+// Get logs for one user
+//----------------------------------------------------------------------------
+async function getUserLogs(userids) {
+
+    userids.forEach( (userid) => {
+        if (verbose) logger.infos('Browse the userlogs collection with user ID : ' + userid);
+        (async() => {
+            let querylog = userLog.find({});
+            querylog.select('email action timestamp ip severity').sort({timestamp: -1});
+            querylog.select().where('userid').equals(userid);
+            await querylog.exec( function (err, loglist) {
+                if (err) console.log(err);
+                if (verbose) logger.infos('Found ' + loglist.length + ' entries\n\n');
+                loglist.forEach((value, index) => {
+                    // Some formatting
+                    let IP = 'Not collected'.padEnd(24, ' ');
+                    if(value.ip) {
+                        IP = value.ip.padEnd(24, ' ');
+                    }
+                    console.log('%s %s %s %s', helpers.convertDateTime(value.timestamp), 
+                                IP,
+                                value.action.padEnd(35, ' '),
+                                value.email);
+                });
+            });
+        })();    
+    });
 }
 
